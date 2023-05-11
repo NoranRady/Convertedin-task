f7dc5e115c3eeeddee62b0a30ed3f321fbbe912e
NoranRady <noran.rady@maxab.io>
2023-05-11 04:01:13 +0300

    Change statistics api returned data

--- app/Repositories/StatisticRepository.php
+++ app/Repositories/StatisticRepository.php
@@ -9,9 +9,14 @@ class StatisticRepository
     public function getTopUsersWithTaskCounts($count)
     {
         return Statistic::with('user')
-            ->orderBy('task_count', 'desc')
-            ->take($count)
-            ->get()
-            ->pluck('user');
+        ->orderBy('task_count', 'desc')
+        ->take(10)
+        ->get()
+        ->map(function($stat) {
+            return [
+                'user_name' => $stat->user->name,
+                'task_count' => $stat->task_count,
+            ];
+        });
     }
 }
