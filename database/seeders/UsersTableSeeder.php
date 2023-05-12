0b62584f7f4057a06ca6364d527bc19f60f41933
NoranRady <noran.rady@maxab.io>
2023-05-12 23:34:09 +0300

    Improve users table seeding performance

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -2,10 +2,11 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\Hash;
 use App\Models\User;
 use Illuminate\Support\Str;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 
 class UsersTableSeeder extends Seeder
@@ -15,19 +16,36 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
+        $connection = DB::connection('mysql');
+
         // Create 10,000 users
-        User::factory()->count(10000)->create();
+        $usersData = [];
+
+        for ($i = 0; $i < 10000; $i++) {
+            $usersData[] = [
+                'name' => 'User ' . ($i + 1),
+                'email' => 'user' . ($i + 1) . '@example.com',
+                'email_verified_at' => now(),
+                'password' => Hash::make('password'),
+                'remember_token' => Str::random(10),
+            ];
+        }
+        
+        User::factory()->connection($connection)->createMany($usersData);
 
         // Create 100 admins
+        $adminsData = [];
         for ($i = 0; $i < 100; $i++) {
-            User::create([
+            $adminsData[] = [
                 'name' => 'Admin ' . ($i + 1),
                 'email' => 'admin' . ($i + 1) . '@example.com',
                 'email_verified_at' => now(),
                 'password' => Hash::make('password'),
                 'is_admin' => true,
                 'remember_token' => Str::random(10),
-            ]);
+            ];
         }
+        
+        $connection->table('users')->insert($adminsData);
     }
 }
